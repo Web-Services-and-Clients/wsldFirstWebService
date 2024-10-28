1201420623d033e0b5dd0a4fdae919d0cb679366
Lwazi <lwazigxogxa@gmail.com>
2024-10-28 14:32:33 +0200

    first commit

---
+++ src/main/java/wsldFirstWebService/WsldFirstWebServiceApplication.java
@@ -0,0 +1,13 @@
+package wsldFirstWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsldFirstWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsldFirstWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsldFirstWebService/WsldFirstWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wsldFirstWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsldFirstWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
